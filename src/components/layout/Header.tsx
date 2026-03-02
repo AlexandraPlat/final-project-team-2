66562a2e483c95486076173a3ee73dafba2fa9b9
Alexandra Plat <alexplatt@mail.ru>
2026-03-02 23:07:52 +0100

    Header edit

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,16 +1,50 @@
 import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 export default function Header() {
   return (
     <header className="header">
-      <div className="logo">Tech Store</div>
-      <nav>
-        <ul className="nav-list">
-          <li><NavLink to="/" end>Home</NavLink></li>
-          <li><NavLink to="/catalog">Catalog</NavLink></li>
-          <li><NavLink to="/cart">Cart</NavLink></li>
-        </ul>
-      </nav>
+      <div className="container header__inner">
+        <div className="logo">Tech Store</div>
+
+        <nav className="nav">
+          <ul className="nav__list">
+            <li>
+              <NavLink
+                to="/"
+                end
+                className={({ isActive }) =>
+                  isActive ? "nav__link active" : "nav__link"
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink
+                to="/catalog"
+                className={({ isActive }) =>
+                  isActive ? "nav__link active" : "nav__link"
+                }
+              >
+                Catalog
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink
+                to="/cart"
+                className={({ isActive }) =>
+                  isActive ? "nav__link active" : "nav__link"
+                }
+              >
+                Cart
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
