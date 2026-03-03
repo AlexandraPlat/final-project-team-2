d8c9b932fb98e481c6c10e598d260ce9f23f1b41
Alexandra Plat <alexplatt@mail.ru>
2026-03-03 11:26:55 +0100

    button add

--- src/components/Homepage/CallToAction.tsx
+++ src/components/Homepage/CallToAction.tsx
@@ -2,8 +2,10 @@ import type { JSX } from "react";
 import Button from "./Button/Button"
 import Container from "./Container";
 import styles from "./CallToAction.module.css";
+import { useNavigate } from "react-router-dom";
 
 export default function CallToAction(): JSX.Element {
+  const navigate = useNavigate();
   return (
     <section className={styles.cta}>
       <Container>
@@ -15,7 +17,8 @@ export default function CallToAction(): JSX.Element {
           Explore our catalog and discover cutting-edge products.
         </p>
 
-        <Button variant="light">
+        <Button variant="light"
+        onClick={() => navigate("/catalog")}>
           View Catalog
         </Button>
       </Container>
