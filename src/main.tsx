0157c107b0fc127cbb0733900869faed1dfdcb42
AlexandraPlat <alexplatt@mail.ru>
2026-03-03 10:51:29 +0100

    Update main.tsx
    delete double import

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,8 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App.tsx";
-import App from "./App.tsx";
 import { Provider } from "react-redux";
-import { store } from "./app/store.ts";
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
