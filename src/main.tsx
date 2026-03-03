96e9be5af2c0b0d2e00da6ffe2d3a7ae3125ad3d
AlexandraPlat <alexplatt@mail.ru>
2026-03-03 10:53:39 +0100

    Update main.tsx
    double import correction

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
-import App from "./App.tsx";
-import { Provider } from "react-redux";
 
+import App from "./App";
+import { store } from "./app/store";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </StrictMode>,
+  </StrictMode>
 );
 
 /* 
