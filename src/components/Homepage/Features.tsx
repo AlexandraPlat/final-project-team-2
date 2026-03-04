a6aa2857ed628dc93a28d79de20bba62556f4995
Alexandra Plat <alexplatt@mail.ru>
2026-03-04 11:32:41 +0100

    Features edition

--- src/components/Homepage/Features.tsx
+++ src/components/Homepage/Features.tsx
@@ -1,11 +1,32 @@
 import FeatureItem from "./FeatureItem";
 import Container from "./Container";
+import { Link } from "react-router-dom";
 
 const features = [
-  { icon: "🚚", title: "Free Delivery", desc: "On orders over $50" },
-  { icon: "🔒", title: "Secure Payments", desc: "100% protected" },
-  { icon: "🔄", title: "Easy Returns", desc: "30-day return policy" },
-  { icon: "💬", title: "24/7 Support", desc: "We are here to help" }
+  { 
+    icon: "🚚", 
+    title: "Free Delivery", 
+    desc: "On orders over $50",
+    link: "/free-delivery"
+  },
+  { 
+    icon: "🖥️", 
+    title: "About us", 
+    desc: "Our guarantee",
+    link: "/about"
+  },
+  { 
+    icon: "🔄", 
+    title: "Easy Returns", 
+    desc: "30-day return policy",
+    link: "/returns"
+  },
+  { 
+    icon: "💬", 
+    title: "24/7 Support", 
+    desc: "We are here to help",
+    link: "/support"
+  }
 ];
 
 export default function Features() {
@@ -14,7 +35,9 @@ export default function Features() {
       <Container>
         <div className="features__grid">
           {features.map((item, index) => (
-            <FeatureItem description={""} key={index} {...item} />
+            <Link to={item.link} key={index} className="features__link">
+              <FeatureItem description={""} {...item} />
+            </Link>
           ))}
         </div>
       </Container>
